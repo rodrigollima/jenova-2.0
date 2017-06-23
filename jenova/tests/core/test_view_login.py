c0803a088aed4c0569d1b72724905fa64b32181f
Rodrigo Lima <rodrigo.lima@inova.net>
2017-06-23 16:52:58 -0300

    login tests

---
+++ jenova/tests/core/test_view_login.py
@@ -0,0 +1,29 @@
+from django.test import TestCase
+
+class GetLoginTest(TestCase):
+    def setUp(self):
+        self.response = self.client.get('/login')
+
+    def test_get(self):
+        self.assertEqual(200, self.response.status_code)
+
+    def test_template(self):
+        self.assertTemplateUsed(self.response, 'login.html')
+
+    def test_html(self):
+        tags = (
+            ('<form', 1),
+            ('<input', 3),
+            ('<button', 1),
+            ('type="email"',1),
+            ('type="checkbox"',1),
+            ('type="password"',1),
+            ('type="submit"',1)
+        )
+
+        for text, count in tags:
+            with self.subTest():
+                self.assertContains(self.response, text, count)
+
+    def test_csrf(self):
+        self.assertContains(self.response, 'csrfmiddlewaretoken')
