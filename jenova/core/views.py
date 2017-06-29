3540020065c887b901fbb27cf59cd9e603b806ba
Leandro-B-P <leandrobatistapereira98@gmail.com>
2017-06-29 15:13:20 -0300

    Update all templates

--- jenova/core/views.py
+++ jenova/core/views.py
@@ -1,10 +1,14 @@
 from django.contrib.auth import authenticate
 from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
-from jenova.core.forms import LoginForm
 # Create your views here.
+
 
 #@login_required(login_url='/login')
 def dashboard(request):
     return render(request, 'dashboard/index.html')
+
+
+def dashboard2(request):
+    return render(request, 'dashboard/index2.html')
 
